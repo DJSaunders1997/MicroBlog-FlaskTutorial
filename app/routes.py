b0c96ab782c38c31b56edc5085ebee5d6c4051e9
Dave <DJSaunders1997@gmail.com>
2020-09-30 14:34:57 +0100

    Completed part 2, changing html pages

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,32 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return 'Hello, World!'
+	user = {'username': 'Jacques'}
+	posts = [
+		{
+			'author': {'username': 'Megan'},
+			'body':	'Isn\'t Newport a beautiful place!'
+		},
+		{
+			'author': {'username': 'Boo'},
+			'body':	'I love my new owners!'
+		},
+		{
+			'author': {'username': 'Jerry'},
+			'body':	'I\'ve gone feral👿! '
+		},
+		{
+			'author': {'username': 'Dave'},
+			'body':	'What on earth should I do with my life now that my dissertation is done? '
+		}
+	]
+
+	# render_template used to change html pages.
+	return render_template('index.html', title='Home', user=user, posts=posts)
+
 
 # This modifies the test view function with a decorator
 # The @app.route decorator creates an association between the argument URL and the function.
