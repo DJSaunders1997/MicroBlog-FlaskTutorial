4b95f07dce565e293629e8f963b45a3a07c9294d
Dave <DJSaunders1997@gmail.com>
2020-09-30 15:59:40 +0100

    LoginForm created

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
@@ -27,6 +28,10 @@ def index():
 	# render_template used to change html pages.
 	return render_template('index.html', title='Home', user=user, posts=posts)
 
+@app.route('/login')
+def login():
+	form = LoginForm()
+	return render_template('login.html', title='Sign In', form=form)
 
 # This modifies the test view function with a decorator
 # The @app.route decorator creates an association between the argument URL and the function.
