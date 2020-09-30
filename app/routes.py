a9e862b69f2d49a8388ebcb8ab3059944ee6be54
Dave <DJSaunders1997@gmail.com>
2020-09-30 17:21:28 +0100

    Completed part 3: webforms

--- app/routes.py
+++ app/routes.py
@@ -33,7 +33,7 @@ def login():
 	form = LoginForm()
 	if form.validate_on_submit():
 		flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
-		return redirect('/index')
+		return redirect(url_for('index'))
 	return render_template('login.html', title='Sign In', form=form)
 
 # This modifies the test view function with a decorator
