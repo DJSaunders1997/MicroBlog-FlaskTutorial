40f2605a8891939cda937271b0c50b005b4acec8
Dave <DJSaunders1997@gmail.com>
2020-10-04 20:16:46 +0100

    VScode Command Palette Conv indent to spaces

--- app/routes.py
+++ app/routes.py
@@ -10,43 +10,43 @@ from werkzeug.urls import url_parse
 @app.route('/index')
 @login_required
 def index():
-	user = {'username': 'Jacques'}
-	posts = [
-		{
-			'author': {'username': 'Megan'},
-			'body':	'Isn\'t Newport a beautiful place!'
-		},
-		{
-			'author': {'username': 'Boo'},
-			'body':	'I love my new owners!'
-		},
-		{
-			'author': {'username': 'Jerry'},
-			'body':	'I\'ve gone feral👿! '
-		},
-		{
-			'author': {'username': 'Dave'},
-			'body':	'What on earth should I do with my life now that my dissertation is done?'
-		}
-	]
+    user = {'username': 'Jacques'}
+    posts = [
+        {
+            'author': {'username': 'Megan'},
+            'body':	'Isn\'t Newport a beautiful place!'
+        },
+        {
+            'author': {'username': 'Boo'},
+            'body':	'I love my new owners!'
+        },
+        {
+            'author': {'username': 'Jerry'},
+            'body':	'I\'ve gone feral👿! '
+        },
+        {
+            'author': {'username': 'Dave'},
+            'body':	'What on earth should I do with my life now that my dissertation is done?'
+        }
+    ]
 
-	# render_template used to change html pages.
-	return render_template('index.html', title='Home Page', posts=posts)
+    # render_template used to change html pages.
+    return render_template('index.html', title='Home Page', posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	if current_user.is_authenticated:
-		return redirect(url_for('index'))
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
 
-	form = LoginForm()
+    form = LoginForm()
 
-	if form.validate_on_submit():
-		user = User.query.filter_by(username=form.username.data).first()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
 
-		if user is None or not user.check_password(form.password.data):
-        	flash('Invalid username or password')
-			return redirect(url_for('login'))
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
 
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
@@ -60,12 +60,12 @@ def login():
 
 @app.route('/logout')
 def logout():
-	logout_user()
-	return redirect(url_for('index'))
+    logout_user()
+    return redirect(url_for('index'))
 
 # This modifies the test view function with a decorator
 # The @app.route decorator creates an association between the argument URL and the function.
 @app.route('/test')
 @app.route('/davetest')
 def test():
-	return 'Testing to see if this works'
+    return 'Testing to see if this works'
