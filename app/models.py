5bf5954645c26193af62a47fa7e9053ad52aaeea
Dave <DJSaunders1997@gmail.com>
2020-10-04 20:28:32 +0100

    Added user susan

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from app import db
 from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixins
+from flask_login import UserMixin
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
