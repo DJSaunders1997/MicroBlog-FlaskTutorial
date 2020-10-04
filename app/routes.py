34b7021ea28ab03a0e3c41ce15ab7faac4e045bf
Dave <DJSaunders1997@gmail.com>
2020-10-04 20:14:28 +0100

    autopep8 routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,10 @@
 from flask import render_template, flash, redirect, request
 from app import app
 from app.forms import LoginForm
-from flask_login import current_user, login_user
-from flask_login import logout_user
-from flask_login import login_required
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
 from werkzeug.urls import url_parse
+
 
 @app.route('/')
 @app.route('/index')
@@ -32,7 +31,8 @@ def index():
 	]
 
 	# render_template used to change html pages.
-	return render_template('index.html', title='Home', user=user, posts=posts)
+	return render_template('index.html', title='Home Page', posts=posts)
+
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -40,16 +40,17 @@ def login():
 		return redirect(url_for('index'))
 
 	form = LoginForm()
+
 	if form.validate_on_submit():
-		 user = User.query.filter_by(username=form.username.data).first()
+		user = User.query.filter_by(username=form.username.data).first()
 
 		if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
+        	flash('Invalid username or password')
+			return redirect(url_for('login'))
 
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
-		
+
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
 
