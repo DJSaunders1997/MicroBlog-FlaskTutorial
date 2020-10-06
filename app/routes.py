73537e09b645d8afa72f67fb220c7a868389b602
Dave <DJSaunders1997@gmail.com>
2020-10-06 21:10:16 +0100

    Added profile routes and html

--- app/routes.py
+++ app/routes.py
@@ -77,6 +77,21 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
+
+# <> in the route url is dynamic
+# flask will accept any text in that 
+# invokes view function with text as argument
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author':user, 'body': 'Test post #1'}
+        {'author':user, 'body': 'Test post #1'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
+
+
 # This modifies the test view function with a decorator
 # The @app.route decorator creates an association between the argument URL and the function.
 @app.route('/test')
